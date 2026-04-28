d2d29c9c98c9c5710b72b73f83f3c681d9d4bcff
Nicholas Sixbury <nicholas.sixbury@usda.gov>
2026-04-28 09:53:57 -0500

    Removed unused imports

--- src/View/MainWindow.java
+++ src/View/MainWindow.java
@@ -20,10 +20,8 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.Cursor;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -41,7 +39,6 @@ import SimpleResult.SimpleResult;
 import Utils.Constants;
 import Utils.Result;
 import Utils.Result.ResultType;
-import ij.IJ;
 import ij.ImagePlus;
 
 /**
